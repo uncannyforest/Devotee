0fabbea1efc091b54b82295908522ad4433513b9
Uncanny Forest <git@uncannyforest.com>
2023-03-28 21:28:28 -0400

    Add main menu (currently nonfunctional)

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public static void NewGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // OnTerrainReady(Terrain.GenerateNewWorld);
+    }
+
+    public static void LoadGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // OnTerrainReady(MapPersistence.LoadGame);
+    }
+
+    public static void OnTerrainReady(Action action) {
+        // SceneManager.sceneLoaded += (s, l) => {Terrain.I.Started += action;};
+    }
+}

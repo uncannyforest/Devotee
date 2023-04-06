f5bfebc677f14648241b74a0a8b8f8fa20db95af
Uncanny Forest <git@uncannyforest.com>
2023-04-05 22:43:24 -0400

    Improve Hill and Rockwall pieces

--- Assets/Land/OpeningAt10.cs
+++ Assets/Land/OpeningAt10.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class OpeningAt10 : MonoBehaviour {
     public GameObject door;
+    public GameObject passage;
+
 
     void Start() {
         if (transform.localScale.y == 10)
             GameObject.Destroy(door);
+        else
+            GameObject.Destroy(passage);
     }
 }

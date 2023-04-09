8f07c25a8ebf12483ba044aa063e88ea96de25d2
Uncanny Forest <git@uncannyforest.com>
2023-04-08 23:58:51 -0400

    Split Hall doors into individual pieces

--- Assets/Land/Hall.cs
+++ Assets/Land/Hall.cs
@@ -3,41 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hall : MonoBehaviour {
-    public GameObject inner1;
-    public GameObject inner2;
-    public GameObject inner3;
-    public GameObject outer1;
-    public GameObject outer2;
-    public GameObject outer3;
     public Material oneWayOpen;
     public Material oneWayClosed;
 
     void Start() {
+        Transform inner = transform.Find("Inner");
+        Transform outer = transform.Find("Outer");
+
         int version = GetComponent<Land>().RandomRange(4);
         switch (version) {
             case 0: // all entrances open
-                GameObject.Destroy(inner1);
-                GameObject.Destroy(inner2);
-                GameObject.Destroy(inner3);
-                GameObject.Destroy(outer1);
-                GameObject.Destroy(outer2);
-                GameObject.Destroy(outer3);
+                foreach (Transform child in inner) GameObject.Destroy(child.gameObject);
+                foreach (Transform child in outer) GameObject.Destroy(child.gameObject);
                 break;
             case 1: // 1 entrance open
-                GameObject.Destroy(inner1);
-                GameObject.Destroy(outer1);
+                GameObject.Destroy(inner.GetChild(0).gameObject);
+                GameObject.Destroy(outer.GetChild(0).gameObject);
                 break;
             case 2: // 3 entrances open
-                GameObject.Destroy(inner3);
-                GameObject.Destroy(outer3);
+                GameObject.Destroy(inner.GetChild(0).gameObject);
+                GameObject.Destroy(inner.GetChild(2).gameObject);
+                GameObject.Destroy(inner.GetChild(4).gameObject);
+                GameObject.Destroy(outer.GetChild(0).gameObject);
+                GameObject.Destroy(outer.GetChild(2).gameObject);
+                GameObject.Destroy(outer.GetChild(4).gameObject);
                 break;
             case 3: // magic oneways
-                GameObject.Destroy(outer3);
-                inner1.GetComponent<MeshRenderer>().material = oneWayOpen;
-                inner2.GetComponent<MeshRenderer>().material = oneWayOpen;
-                inner3.GetComponent<MeshRenderer>().material = oneWayClosed;
-                inner1.layer = LayerMask.NameToLayer("Let Player Through");
-                inner2.layer = LayerMask.NameToLayer("Let Player Through");
+                for (int i = 0; i < 6; i++) {
+                    if (i % 2 == 0) {
+                        GameObject.Destroy(outer.GetChild(i).gameObject);
+                        inner.GetChild(i).GetComponent<MeshRenderer>().material = oneWayClosed;
+                    } else {
+                        inner.GetChild(i).gameObject.layer = LayerMask.NameToLayer("Let Player Through");
+                        inner.GetChild(i).GetComponent<MeshRenderer>().material = oneWayOpen;
+                    }
+                }
                 break;
         }
     }

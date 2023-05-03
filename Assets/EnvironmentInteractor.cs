38c236ff2eb4f32b209e46a65cc812c657d704de
Uncanny Forest <git@uncannyforest.com>
2023-05-02 20:11:51 -0400

    Add messages about holding objects

--- Assets/EnvironmentInteractor.cs
+++ Assets/EnvironmentInteractor.cs
@@ -10,6 +10,7 @@ public class EnvironmentInteractor {
     private HoldObject script;
     private Transform playerHoldTransform;
     private HashSet<GameObject> nearObjects = new HashSet<GameObject>();
+    private FlexibleInputDisplay input;
 
     public HashSet<GameObject> NearObjects {
         get => nearObjects;
@@ -18,15 +19,17 @@ public class EnvironmentInteractor {
     public EnvironmentInteractor(HoldObject script, Transform playerHoldTransform) {
         this.script = script;
         this.playerHoldTransform = playerHoldTransform;
+        input = GameObject.FindObjectOfType<FlexibleInputDisplay>();
     }
 
     public void AddInteractableObject(GameObject trigger) {
         nearObjects.Add(GetInteractableObject(trigger));
-        Debug.Log("INTERACT!");
+        input.SetInteractionMessage("pick up");
     }
 
     public void RemoveInteractableObject(GameObject trigger) {
         nearObjects.Remove(GetInteractableObject(trigger));
+        if (nearObjects.Count == 0) input.SetInteractionMessage(null);
     }
 
     private GameObject GetInteractableObject(GameObject trigger) {
@@ -49,6 +52,7 @@ public class EnvironmentInteractor {
                 o => Vector3.Distance(o.transform.position, script.transform.position)
             ).First();
 
+        input.SetInteractionMessage("release");
         closestObject.GetComponent<Holdable>().Hold();
     }
 
@@ -60,6 +64,7 @@ public class EnvironmentInteractor {
                 child.GetComponent<Holdable>().Drop();
             }
         }
+        UpdateViewForNearbyObjects();
     }
 
     public void NotifyHeldObjectReadyToDrop() {
@@ -68,4 +73,13 @@ public class EnvironmentInteractor {
             childPickMeUp.Drop();
         }
     }
+
+    public void UpdateViewForNearbyObjects() {
+        if (nearObjects.Count > 0) {
+            input.SetInteractionMessage("pick up");
+        } else {
+            input.SetInteractionMessage(null);
+        }
+    }
+
 }
---
+++ Assets/UI/FlexibleInputDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlexibleInputDisplay : MonoBehaviour {
+    public GameObject interactNotice1;
+
+    public void UpdateForHeldObject(GameObject heldObject) {
+        SetInteractionMessage("release");
+    }
+
+    public void SetInteractionMessage(string interact1) {
+        SetInteractionMessage(interactNotice1, interact1, "x");
+    }
+
+    private void SetInteractionMessage(GameObject interactNotice, string message, string desktopKey) {
+        if (!string.IsNullOrEmpty(message)) {
+            interactNotice.SetActive(true);
+
+        message = "press <color=white>" + desktopKey + "</color> to <color=white>"
+            + message + "</color>";
+
+
+            interactNotice.transform.Find("Text").gameObject.GetComponent<Text>().text = message;
+        } else {
+            interactNotice.SetActive(false);
+        }
+
+    }
+}
